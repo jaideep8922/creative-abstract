b09583c292b223d4f60c7cbc364c36c21704ed14
jaideep8922 <jgupta324@gmail.com>
2025-08-05 11:40:34 +0530

    all

--- frontend/src/app/component/Home.tsx
+++ frontend/src/app/component/Home.tsx
@@ -1061,7 +1061,7 @@ export default function MainPage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2025 Creative Abstract. All rights reserved.</p>
+            <p>&copy; 2025 Creative Abstract. All rights reserved</p>
           </div>
         </div>
       </footer>
